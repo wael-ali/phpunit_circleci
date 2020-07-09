50d5d0b73403b0faa0d0d6bd69b6a01361e6eee5
Weal Ali <wealali595@gmail.com>
2020-07-09 17:58:27 +0200

    tests assertions conding

---
+++ src/AppBundle/Entity/Dinosaur.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="dinosaurs")
+ */
+class Dinosaur
+{
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $length = 0;
+
+    /**
+     * @return mixed
+     */
+    public function getLength()
+    {
+        return $this->length;
+    }
+
+    /**
+     * @param mixed $length
+     */
+    public function setLength($length)
+    {
+        $this->length = $length;
+    }
+
+}
--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -4,12 +4,25 @@
 namespace Tests\AppBundle\Entity;
 
 
+use AppBundle\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
-    public function testExample()
+    public function testSettingLength()
     {
-        $this->assertTrue(false);
+        $dinosaur = new Dinosaur();
+        $this->assertSame(0, $dinosaur->getLength());
+        $dinosaur->setLength(9);
+        $this->assertSame(9, $dinosaur->getLength());
+
+    }
+
+    public function testDinosaurHasNotSchrunk()
+    {
+        $dinosaur = new Dinosaur();
+        $dinosaur->setLength(15);
+
+        $this->assertGreaterThan(12, $dinosaur->getLength(), 'Did you put it in the washmachine!!');
     }
 }
