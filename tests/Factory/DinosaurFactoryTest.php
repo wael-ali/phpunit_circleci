753b7317cb0da67e35ab19aa558a39975519fb2c
Weal Ali <wealali595@gmail.com>
2020-07-10 09:15:01 +0200

    hooks, skiptest, mark incomplete

--- tests/Factory/DinosaurFactoryTest.php
+++ tests/Factory/DinosaurFactoryTest.php
@@ -10,15 +10,33 @@ use PHPUnit\Framework\TestCase;
 
 class DinosaurFactoryTest extends TestCase
 {
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new DinosaurFactory();
+    }
     public function testItGrowsAVelociaraptor()
     {
-        $factory = new DinosaurFactory();
-        $dinasour = $factory->growVelociaraptor(5);
+        $dinasour = $this->factory->growVelociaraptor(5);
         $this->assertInstanceOf(Dinosaur::class, $dinasour);
         $this->assertIsString($dinasour->getGenus());
         $this->assertSame('Velociaraptor',$dinasour->getGenus());
         $this->assertSame(5,$dinasour->getLength());
 
+    }
+    public function testItGrowsABabzVelociaraptor()
+    {
+        if (!class_exists('Nanny')){
+            $this->markTestSkipped('There is no body to watch the baby after');
+        }
+        $dinasour = $this->factory->growVelociaraptor(1);
+        $this->assertSame(1,$dinasour->getLength());
+
+    }
+    public function testItGrowsATriceraptos()
+    {
+        $this->markTestIncomplete('Waitin for confirms from Genlabs');
     }
 
 }
