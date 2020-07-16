3a5f7d4ab834e0ad9677153d33005a5b391bb937
Weal Ali <wealali595@gmail.com>
2020-07-16 16:11:10 +0200

    fix 1: sqlite table not found

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -25,36 +25,35 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
         /** @var EnclosureBuilderService $enclosureBuilderService */
 //        $enclosureBuilderService = self::$kernel->getContainer()
 //            ->get('test.'.EnclosureBuilderService::class);
-//        $dinoFactory = $this->createMock(DinosaurFactory::class);
-//            $dinoFactory->expects(self::any())
-//            ->method('growFromSpecification')
-//            ->willReturnCallback(function ($spec){
-//                    return new Dinosaur();
-//            })
-//        ;
-//        $enclosureBuilderService = new EnclosureBuilderService(
-//            $this->getEntityManager(),
-//            $dinoFactory
-//        );
-//        $enclosureBuilderService->buildEnclosure();
-//        /** @var EntityManager $em */
-//        $em = $this->getEntityManager();
-//
-//        $count = (int) $em->getRepository(Security::class)->createQueryBuilder('s')
-//            ->select('COUNT(s.id)')
-//            ->getQuery()
-//            ->getSingleScalarResult()
-//        ;
-//
-//        $this->assertSame(1, $count, 'Amount of security systems are no the same');
-//        $count = (int) $em->getRepository(Dinosaur::class)->createQueryBuilder('d')
-//            ->select('COUNT(d.id)')
-//            ->getQuery()
-//            ->getSingleScalarResult()
-//        ;
-//
-//        $this->assertSame(3, $count, 'Amount of Dinosaurs are no the same');
-        $this->assertSame(3, 3);
+        $dinoFactory = $this->createMock(DinosaurFactory::class);
+            $dinoFactory->expects(self::any())
+            ->method('growFromSpecification')
+            ->willReturnCallback(function ($spec){
+                    return new Dinosaur();
+            })
+        ;
+        $enclosureBuilderService = new EnclosureBuilderService(
+            $this->getEntityManager(),
+            $dinoFactory
+        );
+        $enclosureBuilderService->buildEnclosure();
+        /** @var EntityManager $em */
+        $em = $this->getEntityManager();
+
+        $count = (int) $em->getRepository(Security::class)->createQueryBuilder('s')
+            ->select('COUNT(s.id)')
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
+
+        $this->assertSame(1, $count, 'Amount of security systems are no the same');
+        $count = (int) $em->getRepository(Dinosaur::class)->createQueryBuilder('d')
+            ->select('COUNT(d.id)')
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
+
+        $this->assertSame(3, $count, 'Amount of Dinosaurs are no the same');
     }
     private function clearDatabase()
     {
